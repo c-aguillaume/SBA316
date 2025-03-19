caffa23717ceb9ded98a5d66eb7dc3ccb512edb3
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-03-19 13:04:24 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//Make the FAQ an accordian style
+/*Make destination pictures scroll horizantal
+ with left and right arrows 
+*/
+//making a booking form
+//make each section dynamically click
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides_fade");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
+
+let answers = document.querySelectorAll("h4");
+answers.forEach((event) => {
+    event.addEventListener("click", () => {
+        if (event.classList.contains("active")) {
+            event.classList.remove("active");
+        } else {
+            event.classList.add("active");
+        }
+    });
+});
+
+
+var acc = document.getElementsByClassName("faq_question");
+let i;
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
